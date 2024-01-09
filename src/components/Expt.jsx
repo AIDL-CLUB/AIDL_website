3afa5cd31235ff216633f27f6dd43430ce578da8
SHRUTI DALVI <shrutimdalvi14@gmail.com>
2024-01-09 21:29:48 +0530

    experiments section

--- src/components/Expt.jsx
+++ src/components/Expt.jsx
@@ -4,27 +4,7 @@ import Card from "react-bootstrap/Card";
 const Expt = () => {
   return (
     <div>
-      <Card>
-        <Card.Img variant="top" src="image1.jpg" alt="Experiment 1" />
-        <Card.Body>
-          <Card.Title as="h3">Experiment 1</Card.Title>
-          <Card.Text>
-            This is a description of Experiment 1. Add more details as needed.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-
-      <Card>
-        <Card.Img variant="top" src="image2.jpg" alt="Experiment 2" />
-        <Card.Body>
-          <Card.Title as="h3">Experiment 2</Card.Title>
-          <Card.Text>
-            This is a description of Experiment 2. Add more details as needed.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-
-      {/* Repeat similar Card components for Experiment 3, 4, and 5 */}
+     
     </div>
   );
 };
